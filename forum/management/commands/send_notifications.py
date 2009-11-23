bb5b9e5cfd96265cd29903871eb2804d7eea1ee4
Achiel van der Mandele <Achiel van der Mandele@0dffde6f-cf42-4fae-a7de-9449ba76aff5>
2009-11-23 15:35:44 +0000

    updated notifications
    git-svn-id: http://svn/mxcommunity/QnA/trunk@472 0dffde6f-cf42-4fae-a7de-9449ba76aff5

--- forum/management/commands/send_notifications.py
+++ forum/management/commands/send_notifications.py
@@ -7,14 +7,21 @@ class Command(LabelCommand):
     args = "[timespan]"
     label = 'duration that we want to check'
     def handle_label(self, timespan, **options):
-        subscriptions = Subscription.objects.filter(timespan=timespan)
-        print len(subscriptions)
-        print datetime.now()
-        now = datetime.now()
-		
+        subscriptions = Subscription.objects.filter(timespan=timespan).order_by('user')
         f = (datetime.now() - timedelta(minutes=int(timespan)))
         questions = Question.objects.filter(last_activity_at__gte=f)
+
+        print "subs before filteri %s" % subscriptions
+        subs2 = filter( (lambda x: x.question in questions), subscriptions)
+        print "subs after filter %s " % subs2
+        #for s in subscriptions:
+        #    if s.user not in ordered_subs:
+        #        ordered_subs[s.user] = []
+            
+        #    ordered_subs[s.user].append(s)
+        print "found %s subs" % len(subscriptions)
+		
         print "found %s questions" % len(questions)
-        for q in questions:
-            #print "title %s, time %s" % (q.title, q.last_activity_at)
+        for q in questions[:3]:
+            print "title %s, time %s" % (q.title, q.last_activity_at)
             pass
