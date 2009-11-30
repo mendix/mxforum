59e853cbd173d4ec72003faed035faacbab3cb2c
Achiel van der Mandele <Achiel van der Mandele@0dffde6f-cf42-4fae-a7de-9449ba76aff5>
2009-11-30 15:59:52 +0000

    changed json lib to simplejson from django
    git-svn-id: http://svn/mxcommunity/QnA/trunk@580 0dffde6f-cf42-4fae-a7de-9449ba76aff5

--- forum/ssoauth.py
+++ forum/ssoauth.py
@@ -3,17 +3,26 @@ from django.contrib.auth.models import User
 from base64 import b64encode
 import urllib
 from hashlib import sha256
-import simplejson as json
+import logging
+from django.utils import simplejson
+import sys
  
 class SSOModelBackend(object):
 
     def authenticate_token(self, request, token):
         original = "%s%s" % (request.META['REMOTE_ADDR'],  request.META['HTTP_USER_AGENT'])
+        sys.stderr.write("sysdtr original %s " % original)
+        sys.stderr.flush()
+        logging.error("original %s " % original)
         hashed = b64encode(sha256(original).digest())
         requestdata = urllib.urlencode([('token', token), ('a', 'forum'), ('client', hashed)])
+        sys.stderr.write("going to send requestdata %s " % requestdata)
+        logging.error("going to send requestdata %s " % requestdata)
         u = urllib.urlopen("%s/mxid/validatetoken" % settings.MXID_URL, requestdata)
 
-        response = json.decode(u.read())
+        response = simplejson.decode(u.read())
+        sys.stderr.write("called, decoded and closed stream. result %s" % response)
+        logging.error("called, decoded and closed stream. result %s" % response)
         if (response["valid"] == True):
             return response["username"]
         return None
