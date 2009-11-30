2639f57aba96ef2c6699c735761c58c7654559a6
Achiel van der Mandele <Achiel van der Mandele@0dffde6f-cf42-4fae-a7de-9449ba76aff5>
2009-11-30 11:52:21 +0000

    updated error messages
    git-svn-id: http://svn/mxcommunity/QnA/trunk@567 0dffde6f-cf42-4fae-a7de-9449ba76aff5

--- forum/login_views.py
+++ forum/login_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect, HttpResponse
 import settings
 
-def mxid_login(request, token, email, redirect):
+def mxid_login(request, token, email, redirect=settings.LOGIN_REDIRECT_URL):
     user = authenticate(request=request, token=token)
     if user is not None:
         if user.is_active:
@@ -10,12 +10,12 @@ def mxid_login(request, token, email, redirect):
             print "login ok, redirecting to %s" % redirect
             return HttpResponseRedirect(redirect)
         else:
-            return HttpResponseRedirect("/error")
+            return HttpResponseRedirect("/error?error=User%20isnt%20active")
     else:
-        return HttpResponseRedirect("/error")
+        return HttpResponseRedirect("/error?error=User%20doesnt%20have%20an%20account%20for%20the%20forum")
 
 def login_redirect(request):
 	if 'next' in request.GET:
-		return HttpResponseRedirect("%smxid/login?a=forum&cont=%s" % (settings.MXID_URL, request.GET['next']))
+		return HttpResponseRedirect("%s/mxid/login?a=forum&cont=%s" % (settings.MXID_URL, request.GET['next']))
         
-	return HttpResponseRedirect("%smxid/login?a=forum" % settings.MXID_URL)
+	return HttpResponseRedirect("%s/mxid/login?a=forum" % settings.MXID_URL)
