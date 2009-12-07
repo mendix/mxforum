da27488190e6a4fcf263f80f2b3e6f39465a4f33
Achiel van der Mandele <Achiel van der Mandele@0dffde6f-cf42-4fae-a7de-9449ba76aff5>
2009-12-07 09:26:36 +0000

    finished notifications cronjob
    git-svn-id: http://svn/mxcommunity/QnA/trunk@636 0dffde6f-cf42-4fae-a7de-9449ba76aff5

--- forum/management/commands/send_notifications.py
+++ forum/management/commands/send_notifications.py
@@ -2,26 +2,38 @@ from django.core.management.base import LabelCommand
 from forum.models import Question, Subscription
 from django.db.models import Q
 from datetime import datetime, timedelta
+import settings
+from django.core.mail import send_mail
 
 class Command(LabelCommand):
     args = "[timespan]"
     label = 'duration that we want to check'
     def handle_label(self, timespan, **options):
-        subscriptions = Subscription.objects.filter(timespan=timespan).order_by('user')
+        all_subscriptions = Subscription.objects.filter(timespan=timespan).order_by('user')
         f = (datetime.now() - timedelta(minutes=int(timespan)))
         questions = Question.objects.filter(last_activity_at__gte=f)
 
-        print "subs before filteri %s" % subscriptions
-        subs2 = filter( (lambda x: x.question in questions), subscriptions)
-        print "subs after filter %s " % subs2
-        #for s in subscriptions:
-        #    if s.user not in ordered_subs:
-        #        ordered_subs[s.user] = []
-            
-        #    ordered_subs[s.user].append(s)
+        subscriptions = filter( (lambda x: x.question in questions), all_subscriptions)
+        print "subs after filter %s " % subscriptions
+        ordered_subs = {}
+        for s in subscriptions:
+            if s.user not in ordered_subs:
+                ordered_subs[s.user] = []
+           
+            ordered_subs[s.user].append(s)
         print "found %s subs" % len(subscriptions)
+        print "found %s orderedsubs " % len(ordered_subs)
 		
-        print "found %s questions" % len(questions)
-        for q in questions[:3]:
-            print "title %s, time %s" % (q.title, q.last_activity_at)
-            pass
+        print ordered_subs
+        original_header = "Dear %s,\n\nYou are receiving this message because you have subscribed to receive mail whenever any of your questions have activity. The following questions have seen activity since the last mail digest that you have received from us:\n\n"
+        original_footer = "You can unsubscribe to any of these questions by updating your profile at %s/users/%s?sort=subscriptions\n\nKind regards,\nthe MxForum Team"
+        for user in ordered_subs.iterkeys():
+            message = original_header % user.real_name
+            for s in ordered_subs[user]:
+                q = s.question
+                message += "%s\n%s%s last activity by %s at %s\n\n" % (q.title, settings.MY_URL, q.get_absolute_url(), q.last_activity_by.real_name, q.last_activity_at)
+            message += original_footer % (settings.MY_URL, user.id)
+            send_mail('MxForum updates digest', message, 'mxforum@mendix.com', ['achiel.van.der.mandele@mendix.com'], fail_silently=False)
+            print message
+
+
