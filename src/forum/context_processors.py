779552da9c5460e53c583ea6ab7ae12ae3bc1940
Robert van 't Hof <robert.van.thof@mendix.com>
2015-02-13 14:18:17 +0100

    Replaced the hardcoded urls with variables These have been added to the local_settings.py on mxforum-accp

--- src/forum/context_processors.py
+++ src/forum/context_processors.py
@@ -10,3 +10,15 @@ def footer_json_mxwww_url(request):
 
 def mxappstore_url(request):
 	return {'mxappstore_url' : settings.MXAPPSTORE_URL}
+
+def mxsprintr_url(request):
+	return {'mxsprintr_url' : settings.MXSPRINTR_URL}
+
+def mxdevsite_url(request):
+	return {'mxdevsite_url' : settings.MXDEVSITE_URL}
+
+def mxconfl_url(request):
+	return {'mxconfl_url' : settings.MXCONFL_URL}
+
+def mxacademy_url(request):
+	return {'mxacademy_url' : settings.MXACADEMY_URL}
