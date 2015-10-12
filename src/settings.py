a2951cb45dec0f934b4ed8fc68156c389b3fc052
Robert <robert.van.thof@mendix.com>
2015-10-12 15:53:56 +0200

    Now uses a more supported timezone

--- src/settings.py
+++ src/settings.py
@@ -65,7 +65,7 @@ CELERY_RESULT_SERIALIZER = 'json'
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/WET'
+TIME_ZONE = 'Europe/Amsterdam'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
