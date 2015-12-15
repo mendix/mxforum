7ab003c2d8767c47424ce76afd45962ad24c786e
Robert van 't Hof <robert.van.thof@mendix.com>
2015-12-15 09:38:06 +0100

    Flylogger now has try catch

--- src/forum/flylogger.py
+++ src/forum/flylogger.py
@@ -5,10 +5,14 @@ import os
 def flog(string):
     path = ''
     # Windows
-    if os.name == 'nt':
-        path = os.path.abspath(os.path.join('src', 'forum.log'))
-    else:
-        path = '%s/forum.log' % settings.SITE_SRC_ROOT
-
-    with open(path, 'a+') as f:
-        f.write( "[%s] %s\n\n" % (datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S"), string))
+    try:
+        if os.name == 'nt':
+            path = os.path.abspath(os.path.join('src', 'forum.log'))
+        else:
+            path = '%s/forum.log' % settings.SITE_SRC_ROOT
+    
+        with open(path, 'a+') as f:
+            f.write( "[%s] %s\n\n" % (datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S"), string))
+            
+    except:
+        pass
